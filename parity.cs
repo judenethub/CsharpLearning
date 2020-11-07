4a6496a38d441ee7634a6bf0c5221a7757d56e7c
judenethub <60697235+judenethub@users.noreply.github.com>
2020-11-08 00:37:41 +0300

    Add files via upload
    Parity

---
+++ parity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)  {   // является ли число четным
+
+
+
+            //int parity = Convert.ToInt32(Console.ReadLine());
+
+            //  int c = parity | 1;
+            //   if ((parity + 1) == c )
+            //     Console.WriteLine("Четное");
+            //   else
+            //      Console.WriteLine("нечетное");
+
+
+
+            // int c = parity % 2;
+
+            // if(c == 0)
+            //     Console.WriteLine("Четное");
+            //else
+            //    Console.WriteLine("нечетное");
+
+            Console.WriteLine("Введите число для проверки:");
+            byte x = Convert.ToByte(Console.ReadLine());
+            byte n = (byte)(x << 7);
+
+            if (n == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное");
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
