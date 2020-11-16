381ff0c922d61c44662231e1df914537064ddd43
judenethub <60697235+judenethub@users.noreply.github.com>
2020-11-16 13:51:47 +0300

    Add files via upload

---
+++ exchange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    class Program
+    {   
+        static void Converter(double a, double b)
+        {
+            Console.WriteLine("{0} рублей по курсу {1} стоят {2}", a, b, (a * b));
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Напишите сумму");
+            double conver = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Напишите курс");
+            double exchange = Convert.ToDouble(Console.ReadLine());
+            Converter(conver, exchange);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
