6037919974ec38f750323e118ca9727bee846b41
judenethub <60697235+judenethub@users.noreply.github.com>
2020-11-19 21:29:14 +0300

    Add files via upload

---
+++ RecursionFactorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    class Program
+    {
+      
+        static int Calculate(int a)
+        {
+            
+            if (a == 1) { return 1; }
+            else { return a * Calculate(a-1); }
+
+            
+
+        }
+        static void Main()
+        {
+            
+            int a = Calculate(5);
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+
+    }
+}
