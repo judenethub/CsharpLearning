82e6677c7931a7643cd62d7b3bfaa741e58769bf
judenethub <60697235+judenethub@users.noreply.github.com>
2020-11-30 21:51:41 +0300

    Add files via upload

---
+++ interpolation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            string result = $"Hello {surname} {name}!\nIt`s a good day today! ";
+            Console.WriteLine(result);
+            
+            Console.ReadKey();
+        }
+
+    }
+}
