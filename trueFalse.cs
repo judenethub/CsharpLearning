b3968f64799f1b6474da7380376ab32ffe000be9
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-01 23:04:37 +0300

    Add files via upload

---
+++ trueFalse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            bool pensioner = false, zanyatost = false, job = true ;
+            
+            
+
+            bool subsidiya = (pensioner ^ zanyatost ^ job) & ( pensioner & zanyatost ^ job );
+            Console.WriteLine(subsidiya);
+            Console.ReadKey();
+        }
+
+    }
+}
