07b4217682ea4603b1826ffbd730fbe7556ce594
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-02 22:50:56 +0300

    Add files via upload

---
+++ simplestcrypt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            char rand = Convert.ToChar(Console.ReadLine());  
+
+            int mask = 216;
+
+            char changed = Convert.ToChar(rand ^ mask);
+
+            char unchanged = Convert.ToChar(changed ^ mask);
+
+
+
+            Console.WriteLine($"{rand} меняется маской {mask} на {changed} и обратно  становится {unchanged}   ");
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
