3776949c2770c00d85c93b9e73f3ec7c6a5b98ce
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-08 00:24:54 +0300

    Add files via upload

---
+++ triangle2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    static class Program
+    {
+        static void Main()
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            
+            if ((x < (y + z)) && (y < (x + z)) && (z < (x + y)))
+            {
+                Console.Write("треугольник ");
+                if (Math.Pow(x,2) + Math.Pow(y,2) == Math.Pow(z,2) | Math.Pow(x, 2) + Math.Pow(z, 2) == Math.Pow(y, 2) | Math.Pow(y, 2) + Math.Pow(z, 2) == Math.Pow(x, 2))
+                {
+                    Console.Write(" прямоугольный");
+                }
+                if ( x == y && y == z)
+                {
+                    Console.Write(" равносторонний");
+                }
+                if ( (x == y & z != x) | (x == z & y != x) | (z == y & z != x))
+                {
+                    Console.Write(" равнобедренный");
+                }
+                Console.WriteLine(y);
+
+            }
+            else
+            {
+                Console.WriteLine("треугольник НЕ существует");
+            }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
