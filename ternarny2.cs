c7f22ea9d718b5ba5cc8132f10586483d6170374
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-10 19:18:42 +0300

    Add files via upload

---
+++ ternarny2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            (x >= y ? ref x : ref y) = 100;
+            Console.WriteLine($"{x}    {y}");
+            Console.ReadKey();
+        }
+
+    }
+}
