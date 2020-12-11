32ade9f45a82fe3f94b2852bc8bd9f2a20d3a074
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-11 14:37:32 +0300

    Add files via upload

---
+++ whileWithoutFor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loop
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (x < y) { while (x != y - 1) { x++; if (x % 2 != 0) { Console.WriteLine(x); } } }
+            else if (x > y) { while (x != y + 1) { x--; if (x % 2 != 0) { Console.WriteLine(x); } } }
+            else { Console.WriteLine("равны"); }
+
+            Console.ReadKey();
+        }
+
+    }
+}
