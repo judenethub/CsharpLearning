280f0b57866513e3be786472af25c8cf9c1d2c66
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-11 15:08:44 +0300

    Update whileWithoutFor.cs

--- whileWithoutFor.cs
+++ whileWithoutFor.cs
@@ -10,12 +10,12 @@ namespace Loop
         {
             int x = Convert.ToInt32(Console.ReadLine());
             int y = Convert.ToInt32(Console.ReadLine());
+            if (x > y) { (x, y) = (y, x); }
+            while (x != y - 1) { x++; if (x % 2 != 0) { Console.WriteLine(x); } }
 
-
-
-            if (x < y) { while (x != y - 1) { x++; if (x % 2 != 0) { Console.WriteLine(x); } } }
-            else if (x > y) { while (x != y + 1) { x--; if (x % 2 != 0) { Console.WriteLine(x); } } }
-            else { Console.WriteLine("равны"); }
+           // if (x < y) { while (x != y - 1) { x++; if (x % 2 != 0) { Console.WriteLine(x); } } }
+            //else if (x > y) { while (x != y + 1) { x--; if (x % 2 != 0) { Console.WriteLine(x); } } }
+           // else { Console.WriteLine("равны"); }
 
             Console.ReadKey();
         }
