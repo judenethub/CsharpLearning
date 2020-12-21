a42ad413b010ea434a8b25f889b12d6868e214b3
judenethub <60697235+judenethub@users.noreply.github.com>
2020-12-21 15:01:18 +0300

    Update ArrayThreeDimensional.cs

--- ArrayThreeDimensional.cs
+++ ArrayThreeDimensional.cs
@@ -7,30 +7,44 @@ namespace ConsoleApp1
     {
         public static void Main(String[] args)
         {
-             string[,,] biblio = new string[5, 10, 10];
-            biblio[1, 5, 8] = "Буратино";
-            biblio[3, 6, 2] = "Алиса";
-            biblio[4, 4, 0] = "Шапка";
-            Console.WriteLine("Название книги");
-            string userBook = Console.ReadLine();
+            string[,,,] biblio = new string[3, 5, 10, 10];
+            biblio[0, 3, 8, 5] = "Буратино";
+            biblio[1, 2, 6, 1] = "Пеппа";
+            biblio[2, 4, 1, 3] = "CLR";
+            int k = 0;
 
-            for (int z = 0; z < biblio.GetLength(0); z++)
+            string userWant = Console.ReadLine();
+
+            string[] all = new string[20];
+            for (int w = 0; w < biblio.GetLength(0); w++)
             {
-                for (int y = 0; y < biblio.GetLength(1); y++)
+                for (int z = 0; z < biblio.GetLength(1); z++)
                 {
-                    for (int x = 0; x < biblio.GetLength(2); x++)
+                    for (int y = 0; y < biblio.GetLength(2); y++)
                     {
-                        if (biblio[z, y, x] == userBook )
+                        for (int x = 0; x < biblio.GetLength(3); x++)
                         {
-                            Console.WriteLine($"книга {userBook} находится на {z} стеллаже, {y} полке и {x} слева ");
+                            if (biblio[w, z, y, x] == userWant)
+                            {
+                                Console.WriteLine( $"Книга {userWant} находится в {w}  кмнате  на {z} стеллаже  на {y} полке {x} слева  ");
+                            }
+                            if (biblio[w,z,y,x] != null & biblio[w, z, y, x] != userWant) 
+                            {
+                                
+                                    all[k] = biblio[w, z, y, x];
+                                    k++;
+                                
+                            }
+
                         }
                     }
                 }
             }
-
-
-
-
+            Console.WriteLine("\nТакже имеются книги:\n");
+            for (int w = 0; w < all.Length; w++)
+            {
+                Console.WriteLine(all[w]);
+            }
 
 
             Console.ReadKey();
